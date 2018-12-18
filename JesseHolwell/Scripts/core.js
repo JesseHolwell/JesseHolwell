0c08da01da0c3aacb7754dc9b31fb6269234e32b
Jesse Holwell <jesse.d.holwell@gmail.com>
2018-12-18 15:52:35 +1100

    ux fixes

--- JesseHolwell/Scripts/core.js
+++ JesseHolwell/Scripts/core.js
@@ -1,13 +1,24 @@
 $(function () {
     //$(".headroom").headroom();
 
+    $('a.scroll-link').click(function (event) {
+        event.preventDefault();
+        //do whatever
+    })
+
     $("[id^=navbtn-]").click(function () {
         //scrollTo("#contact");
 
         $([document.documentElement, document.body]).animate({
             scrollTop: $("#" + this.id.split("-")[1]).offset().top
-        }, 2000);
+        }, 1500);
     })
+
+    //$("").click(function () {
+    //    return false;
+    //})
+
+
 })
 
 AOS.init();
